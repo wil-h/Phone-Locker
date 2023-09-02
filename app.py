4e5889d1ab6a26e0e35f9f0dec48fedaf001475a
wil-h <123789183+wil-h@users.noreply.github.com>
2023-09-02 11:38:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1648,7 +1648,10 @@ def AIGenerate():
         #window will close when operations are done
     except Exception as e:
         driver.quit()
-        return(str(e))
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        line_number = exc_tb.tb_lineno
+        estr=str(e)+","+str(line_number)
+        return(str(estr))
         #return render_template('aiisdumb.html')
        
 
