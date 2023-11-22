b6723e0d116e9a982fa5b8c41c5150ce3eaf9045
wil-h <123789183+wil-h@users.noreply.github.com>
2023-11-22 16:19:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -204,12 +204,12 @@ def selenium(IP):
     ydim=0
     x=0
     y=0
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("enable-automation")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--disable-dev-shm-usage")
+    #options.add_argument("--headless")
+    #options.add_argument("--disable-gpu")
+    #options.add_argument("--no-sandbox")
+    #options.add_argument("enable-automation")
+    #options.add_argument("--disable-infobars")
+    #options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     try:
         #load first page
