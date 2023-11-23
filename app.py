61994def19c00bcb35f75502fb0eecfef860b65c
wil-h <123789183+wil-h@users.noreply.github.com>
2023-11-22 19:42:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
