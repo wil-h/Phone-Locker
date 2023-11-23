75ce5333eb7f00edac37fdc109e60ba6a6cbb233
wil-h <123789183+wil-h@users.noreply.github.com>
2023-11-22 19:30:56 -0500

    final fix for sqlite interference bug

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ def selenium(IP):
     ydim=0
     x=0
     y=0
-    #options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("enable-automation")
