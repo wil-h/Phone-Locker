a9fcd9155a102b4e482239d5b9162d823d978141
wil-h <123789183+wil-h@users.noreply.github.com>
2023-12-15 19:51:02 -0500

    Update findstatus.py

--- findstatus.py
+++ findstatus.py
@@ -24,66 +24,74 @@ def write_db(IP, column, insert):
             nothing="nothing"
             done=False
 def findstatus(IP, actionlst):
-    try:
-        actionlist=eval(eval(actionlst))
-        options = Options()
-        options.add_argument("--headless=new")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--no-sandbox")
-        options.add_argument("enable-automation")
-        options.add_argument("--disable-infobars")
-        options.add_argument("--disable-dev-shm-usage")
-        driver = webdriver.Chrome(options=options)
-        ur_l=actionlist[0]
-        driver.get(ur_l)
-        driver.set_window_size(1000,1000)
-        WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-        time.sleep(3)
-        for x in range(1,len(actionlist),2):
-            if actionlist[x]=='T':
-                elem = driver.switch_to.active_element
-                if actionlist[x+1]!='BS':
-                    elem.send_keys(actionlist[x+1])
-                else:
-                    elem.send_keys(Keys.BACKSPACE)
-            if actionlist[x]=='C':
-                WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-                time.sleep(1)
-                actions = ActionChains(driver)
-                body=driver.find_element(By.TAG_NAME, "body")
-                actions.move_to_element(body)
-                l=actionlist[x+1].split(',')
-                actions.move_by_offset(l[0],l[1]).click() 
-                actions.perform()
-                WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-                time.sleep(4)
-        time.sleep(15)
-        if driver.current_url==ur_l:
-            #check for upcoming assingments
-            if ur_l=="https://teams.microsoft.com/_#/apps/66aeee93-507d-479a-a3ef-8f494af43945/sections/classroom":
-                WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-                WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, "//iframe[@title='Assignments Tab View']")))
-                driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe[@title='Assignments Tab View']"))
-                time.sleep(5)
-                days=[]
-                try:
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "date-group-label-shorthand__pjq0w")))
-                    days=driver.find_elements(By.CLASS_NAME, "date-group-label-shorthand__pjq0w")
-                except:
-                    status="false"
-                    days=driver.find_elements(By.CLASS_NAME, "date-group-label-shorthand__pjq0w")
-                for day in days:
-                    if day.text=="Today" or day.text=="Tomorrow":
-                        status="true"
-                        break
-                    status="false"
-        else:
-            status="false"
-        write_db(IP, "STATUS", status)
-        write_db(IP, "WORKING", "done")
-    except Exception as e:
-        write_db(IP, "STATUS", "false")
-        write_db(IP, "WORKING", "done")
+    print("started")
+    actionlist=eval(eval(actionlst))
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("enable-automation")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
+    ur_l=actionlist[0]
+    driver.get(ur_l)
+    driver.set_window_size(1000,1000)
+    WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    time.sleep(3)
+    for x in range(1,len(actionlist),2):
+        if actionlist[x]=='T':
+            elem = driver.switch_to.active_element
+            if actionlist[x+1]!='BS':
+                elem.send_keys(actionlist[x+1])
+            else:
+                elem.send_keys(Keys.BACKSPACE)
+        if actionlist[x]=='C':
+            WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+            time.sleep(1)
+            actions = ActionChains(driver)
+            body=driver.find_element(By.TAG_NAME, "body")
+            actions.move_to_element(body)
+            l=actionlist[x+1].split(',')
+            print(actionlist[x+1])
+            print(l)
+            actions.move_by_offset(l[0],l[1]).click() 
+            actions.perform()
+            WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+            time.sleep(4)
+    print("waiting")
+    time.sleep(15)
+    print("woted")
+    if driver.current_url==ur_l:
+        print("url==url")
+        #check for upcoming assingments
+        if ur_l=="https://teams.microsoft.com/_#/apps/66aeee93-507d-479a-a3ef-8f494af43945/sections/classroom":
+            print("url==teams")
+            WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+            WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, "//iframe[@title='Assignments Tab View']")))
+            driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe[@title='Assignments Tab View']"))
+            print("waited for iframe")
+            time.sleep(5)
+            days=[]
+            try:
+                WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "date-group-label-shorthand__pjq0w")))
+                print("waited for days")
+                days=driver.find_elements(By.CLASS_NAME, "date-group-label-shorthand__pjq0w")
+                print("found days:",days)
+            except:
+                status="false"
+                print("had exception")
+                days=driver.find_elements(By.CLASS_NAME, "date-group-label-shorthand__pjq0w")
+            for day in days:
+                if day.text=="Today" or day.text=="Tomorrow" or day.text=="Friday":
+                    status="true"
+                    break
+                status="false"
+    else:
+        status="false"
+    write_db(IP, "STATUS", status)
+    write_db(IP, "WORKING", "done")
+    print(status)
 while True:
     try:
         with sqlite3.connect('database.db') as conn:
