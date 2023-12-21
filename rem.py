626aba2fad5e8bd1732265cbb414549906d6f1a4
wil-h <123789183+wil-h@users.noreply.github.com>
2023-12-21 15:53:19 -0500

    Update and rename findstatus.py to rem.py

---
+++ rem.py
@@ -0,0 +1,24 @@
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import threading
+import time
+import sqlite3
+
+while True:
+    try:
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM api WHERE ALIST = ?',('',))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("deleted")
+        time.sleep(2)
+    except Exception as e:
+        print(e)
+    
